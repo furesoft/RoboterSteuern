461fc2023190263ae8f963e310e31c6c84d86ace
Chris Anders <filmee24@gmail.com>
2021-02-26 17:55:36 +0100

    Update RoboterSteuern.java

--- src/RoboterSteuern.java
+++ src/RoboterSteuern.java
@@ -1,3 +1,6 @@
+import builders.CeilingBuilder;
+import core.BuilderManager;
+
 public class RoboterSteuern {
     private static WELT myWelt;
 
@@ -5,10 +8,15 @@ public class RoboterSteuern {
     public static void main(String[] args) {
         //Todo: haus bauen
         myWelt = new WELT(6, 6 ,10);
-        IRobotBuilder ceilingBuilder = new CeilingBuilder();
+
 
         BuilderManager manager = new BuilderManager(myWelt);
+
+        IRobotBuilder ceilingBuilder = new CeilingBuilder();
         manager.add(ceilingBuilder);
+
+        IRobotBuilder houseBuilder = new HouseBuilder();
+        manager.add(houseBuilder);
 
         manager.spawn(0);
         manager.run();

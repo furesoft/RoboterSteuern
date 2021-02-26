854a9916ea7873c9c2a257bf0b45ff23a12ebeae
Chris Anders <filmee24@gmail.com>
2021-02-26 17:55:29 +0100

    Update CeilingBuilder.java

--- src/builders/CeilingBuilder.java
+++ src/builders/CeilingBuilder.java
@@ -1,22 +1,37 @@
-public class CeilingBuilder implements  IRobotBuilder {
+package builders;
+
+import core;
+
+public class CeilingBuilder implements IRobotBuilder {
     private ROBOTER _rob;
     private WELT _welt;
 
     private boolean _isInitialized;
+    private BuilderManager _manager;
+
+    private int _counter = 0;
 
     @Override
-    public void init(WELT world) {
+    public void init(WELT world, BuilderManager manager) {
         _isInitialized = true;
 
         _rob = new ROBOTER(world);
         _welt = world;
 
         _rob.LinksDrehen();
+        
+        _manager = manager;
     }
 
     @Override
     public void step() {
         if(_isInitialized) {
+            _counter++;
+
+            if(_counter == _welt.getWeltBreite() * _welt.getWeltLaenge() / 2) {
+                _manager.spawn(1);
+            }
+
             if (_rob.IstBlickNorden()) {
                 if (!_rob.IstWand()) {
                     if (_rob.IstZiegel()) {

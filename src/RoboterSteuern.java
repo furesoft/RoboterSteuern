efde26aebc3b28bc41c57b667f1cfdcf3d31544d
Chris Anders <filmee24@gmail.com>
2021-02-26 14:39:16 +0100

    Add Ceiling

--- src/RoboterSteuern.java
+++ src/RoboterSteuern.java
@@ -3,39 +3,75 @@ import java.awt.*;
 public class RoboterSteuern {
 
 
+    private static WELT myWelt;
+    private static ROBOTER myRob1;
+    private static ROBOTER myRob2;
+
     public static void main(String[] args) {
         //Todo: haus bauen
-        WELT myWelt = new WELT(6, 6 ,25);
-        ROBOTER myRob1 = new ROBOTER(myWelt);
+        myWelt = new WELT(10, 10 ,20);
+        myRob1 = new ROBOTER(myWelt);
 
-        InitRobot(myRob1, 4);
-
-        ROBOTER myRob3 = new ROBOTER(myWelt);
-
-        InitRobot(myRob3, 2);
-
-        ROBOTER myRob2 = new ROBOTER(myWelt);
-
-        InitRobot(myRob2, 0);
-
-        Thread t1 = new Thread(new RoboterThread(myRob1), "robo1");
-        Thread t2 = new Thread(new RoboterThread(myRob2), "robo2");
-        Thread t3 = new Thread(new RoboterThread(myRob3), "robo3");
-
-        t1.start();
-        t2.start();
-        t3.start();
-
+        buildCeiling();
     }
 
-    private static void InitRobot(ROBOTER robot, int startPos) {
-        robot.SprunghoeheSetzen(20);
-        robot.LinksDrehen();
+    private static void buildCeiling() {
+        myRob1.LinksDrehen();
 
-        for (int i = 0; i < startPos; i++) {
-            robot.Schritt();
+        for (int x = 0; x < myWelt.getWeltBreite(); x++) {
+            for (int y = 0; y < myWelt.getWeltLaenge()-1; y++) {
+                myRob1.Hinlegen("grün");
+                myRob1.Schritt();
+
+                if(x == myWelt.getWeltBreite()  / 2 && y == myWelt.getWeltLaenge() /2) {
+                    myRob2 = new ROBOTER(myWelt);
+                    myRob2.LinksDrehen();
+                    myRob2.Schritt();
+                    myRob2.Schritt();
+                    myRob2.RechtsDrehen();
+                    myRob2.Schritt();
+                    myRob2.Schritt();
+                }
+
+                if(myRob1.IstWand()) {
+                    if(myRob1.IstBlickOsten()) {
+                        myRob1.RechtsDrehen();
+                        myRob1.Hinlegen("grün");
+                        myRob1.Schritt();
+                        myRob1.RechtsDrehen();
+                    }
+
+                    else if(myRob1.IstBlickWesten()) {
+                        myRob1.LinksDrehen();
+
+                        if(myRob1.IstBlickSueden() && myRob1.IstWand()) {
+                            myRob1.RechtsDrehen();
+                            myRob1.RechtsDrehen();
+
+                            while(!myRob1.IstWand()) {
+                                if(myRob1.IstZiegel()) {
+                                    myRob1.Schritt();
+                                }
+                                else {
+                                    myRob1.Hinlegen("grün");
+                                }
+                            }
+
+                            break;
+                        }
+
+                        myRob1.Hinlegen("grün");
+                        myRob1.Schritt();
+                        myRob1.LinksDrehen();
+
+                        if(myRob1.IstWand()) {
+                            myRob1.RechtsDrehen();
+                        }
+                    }
+
+
+                }
+            }
         }
-
-        robot.RechtsDrehen();
     }
 }
